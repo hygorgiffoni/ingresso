231c3b24bcd5b7f7d827e8b6d5f3c23ae21b1dfd
hygorgiffoni <hygor.giffoni@gmail.com>
2023-12-18 14:12:27 -0300

    habilita botão comprar

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// função para comprar ingressos
+function comprar() {
+    // identifica o tipo e quantidade
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
+    // subtrair a quantidade comprada da quantidade disponível dos ingressos
+    if(tipo == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');   
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');   
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');   
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');   
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');   
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');   
+    }
+}
